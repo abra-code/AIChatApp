f9b858d10b1a0875596b729c01b82263cc8c9657
Tom <tom-dev@abracode.com>
2025-11-13 01:52:43 -0800

    Update llama.cpp to b7020
    Also add `modify_index_html.sh` for automating modifications on top of original index.html
    AIChat.png can be smaller as we display it as 128x128 pixels only

--- AIChat.app/Contents/Resources/Scripts/webkit.client.js
+++ AIChat.app/Contents/Resources/Scripts/webkit.client.js
@@ -7,5 +7,13 @@
 const loadingDiv = document.getElementById('loading');
 if (loadingDiv)
 {
-	loadingDiv.innerHTML = '<div style="display: block; margin-top: 200px; text-align: center; height: 50vh; align-items: center; justify-content: center;"><p>Large language model is loading. Please wait...</p><progress indeterminate></progress></div>';
+	let innerHtml = '<div style="display: block; margin-top: 200px; text-align: center; height: 50vh; align-items: center; justify-content: center;">' +
+					'<p><b>Large language model is loading. Please wait...</b></p><progress indeterminate></progress><br><br>' +
+					'<p><b>AIChat.app is built with llama-server tool from open source Llama.cpp project</b></p>' +
+					'<p><i>The main goal of llama.cpp is to enable LLM inference with minimal setup and state-of-the-art performance on a wide range of hardware - locally and in the cloud.<br>Apple silicon is a first-class citizen - optimized via ARM NEON, Accelerate and Metal frameworks</i></p><br>' +
+					'<p><b>AIChat.app is built with OMC engine</b></p>' +
+					'<p><i>OnMyCommand is a low code macOS app development environment utilizing command line tools and shell scripts</i></p><br>' +
+					'</div>';
+	
+ 	loadingDiv.innerHTML = innerHtml;
 }
---
+++ WebUI/webui-index.sed
@@ -0,0 +1,4 @@
+s|--background\:oklch\(100% 0 0\)|--background:oklch(90% 0 0)|
+s|--background\:oklch\(16% 0 0\)|--background:oklch(27% 0 0)|
+s|llama\.cpp</h1> *<p class="text-lg text-muted-foreground">How can|</h1><p></p><center><img src="AIChat.png" width="128" height="128"></center><p class="text-lg text-muted-foreground">How can|
+s|llama\.cpp<|AIChat<|g
